b6f3ab56a93a8c23b34c2c165fdf194baaffb24e
Enes Dalga <enesdalgaa@gmail.com>
2018-01-05 23:28:46 +0300

    First commit

---
+++ CoreApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
